39681c48a433bb89ce28938b812440b0ce611f5a
thaseki <tahirhasekii@gmail.com>
2020-02-20 19:24:36 +0300

    fix

--- src/hashCode/Main.java
+++ src/hashCode/Main.java
@@ -5,21 +5,10 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		IOReady.writeFile();
-<<<<<<< HEAD
-		// Test comment
-		// Test2 comment
-		// test3 ee
-	
 		System.out.println("LLLLL");
-		
-		
-	
-=======
-		
 		
 		System.out.println("Deneme out");
 		
->>>>>>> 5cd7f497dafe55a063f6f9779f654106034344a1
 	}
 	
 	

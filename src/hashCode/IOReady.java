b0b4cbb44b484e532944be7842e4048d7ad834da
thaseki <tahirhasekii@gmail.com>
2020-02-20 18:11:06 +0300

    a

--- src/hashCode/IOReady.java
+++ src/hashCode/IOReady.java
@@ -3,6 +3,7 @@ package hashCode;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 
 public class IOReady {
@@ -10,13 +11,25 @@ public class IOReady {
 	public static void ReadFile(String filename ) {
 		String str;
 		try {
-			File file = new File("input.csv");
+			File file = new File(filename);
 			BufferedReader br = new BufferedReader(new FileReader(file));
 			while ((str = br.readLine()) != null) {
-
-				}
+				
+			}
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+	}
+	
+	public static void writeFile() {
+	    try {
+	        FileWriter myWriter = new FileWriter("filename.txt");
+	        myWriter.write("Files in Java might be tricky, but it is fun enough!");
+	        myWriter.close();
+	        System.out.println("Successfully wrote to the file.");
+	      } catch (IOException e) {
+	        System.out.println("An error occurred.");
+	        e.printStackTrace();
+	      }
 	}
 }

e9f95e6884e16b59ca90a91a8ad3e22889e2d8e3
thaseki <tahirhasekii@gmail.com>
2020-02-20 17:54:44 +0300

    Read updated

--- src/hashCode/IOReady.java
+++ src/hashCode/IOReady.java
@@ -1,8 +1,22 @@
 package hashCode;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class IOReady {
 
 	public static void ReadFile(String filename ) {
-		
+		String str;
+		try {
+			File file = new File("input.csv");
+			BufferedReader br = new BufferedReader(new FileReader(file));
+			while ((str = br.readLine()) != null) {
+
+				}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 }

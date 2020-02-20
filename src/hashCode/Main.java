5cd7f497dafe55a063f6f9779f654106034344a1
thaseki <tahirhasekii@gmail.com>
2020-02-20 19:20:36 +0300

    asa

--- src/hashCode/Main.java
+++ src/hashCode/Main.java
@@ -5,9 +5,10 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		IOReady.writeFile();
-		// Test comment
-		// Test2 comment
-		// test3 ee
+		
+		
+		System.out.println("Deneme out");
+		
 	}
 
 }
